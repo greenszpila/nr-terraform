9fea93e1588aefe3d217f80053f123137158f301
Krzysztof <kspikowski@gmail.com>
2023-10-26 17:31:54 +0100

    Re-add variables.tf

---
+++ common/variables/variables.tf
@@ -0,0 +1,23 @@
+# your unique New Relic account ID 
+output "nr_account_id" {
+  value = ""
+}
+# your User API key
+output "nr_api_key" {
+  value = "" 
+}
+
+# valid regions are US and EU
+output "nr_region" {
+  value = "US"
+}
+
+# your unique New Relic App ID 
+output "nr_appname" {
+  value = "r-restaurantService"
+}
+# your unique New Relic account ID for dev used in repalcer json
+output "nr_account_id_dev" {
+  value = ""
+}
+
